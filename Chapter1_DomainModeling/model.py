8667e02973fedb3af039127da20dc238deb93d20
FreyGeospatial <jordan@freygeospatial.com>
2022-08-05 18:00:00 -0400

    created more unit tests

--- Chapter1_DomainModeling/model.py
+++ Chapter1_DomainModeling/model.py
@@ -11,10 +11,33 @@ class OrderLine:
 
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        """
+        Creates a Batch of stock, or inventory, which is modeled as an object. 
+        
+        self.reference is a Batch ID.
+        self.sku is the SKU number for the product
+        self.eta is the estimated time of delivery
+        self.available_quantity is how much inventory of product is available
+        """
+
         self.reference = ref
         self.sku = sku
         self.eta = eta
         self.available_quantity = qty
-    
+
+
+    # If a string is the first thing presented in a function, by
+    # default it becomes the docstring. To access, use `help(Batch.allocate)`
     def allocate(self, line: OrderLine):
+        """
+        remove inventory after order is placed
+        """
         self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        """
+        Do we have the product listed in the system?
+        Do we have the item in stock?
+        """
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
--- Chapter1_DomainModeling/test_batches.py
+++ Chapter1_DomainModeling/test_batches.py
@@ -1,6 +1,7 @@
-from model import OrderLine
-from model import Batch
 from datetime import date
+from typing import Tuple
+
+from model import Batch, OrderLine
 
 # let's assume that we have 20 small tables in inventory and
 # we allocate an order line for 2 of those.
@@ -15,5 +16,33 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
 
     assert batch.available_quantity == 18
 
-# although we've written a lot of code just to subtract one value from another, modeling
-# our domain will probably pay off.
+    # although we've written a lot of code just to subtract one value from another, modeling
+    # our domain will probably pay off.
+
+def make_batch_and_line(sku, batch_qty, line_qty) -> Tuple:
+    """Create a batch object and an order"""
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)        
+    )
+
+def test_can_allocate_if_available_greater_than_require() -> None:
+    """this test should pass, as order line quantity is less than inventory (batch) quantity"""
+    large_batch, small_line = make_batch_and_line(sku="ELEGANT-LAMP", batch_qty=20, line_qty=2)
+    assert large_batch.can_allocate(small_line)
+
+def test_cannot_allocate_if_available_smaller_than_required() -> None:
+    """this test should fail, as the order (line) quantity is larger than inventory (batch)"""
+    small_batch, large_line = make_batch_and_line(sku="ELEGANT-LAMP", batch_qty=2, line_qty=20)
+    assert small_batch.can_allocate(large_line) is False
+
+def test_can_allocate_if_available_equal_to_required() -> None:
+    """This should pass as order line and batch quantities are equal"""
+    batch, line = make_batch_and_line(sku="ELEGANT-LAMP", batch_qty=2, line_qty=2)
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_skus_do_not_match() -> None:
+    """This should assert false as skus do not match"""
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+    assert batch.can_allocate(different_sku_line) is False
