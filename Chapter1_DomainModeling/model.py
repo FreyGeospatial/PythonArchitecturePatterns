23481305cd693de7a15bc89bbaeb03d3233522aa
FreyGeospatial <jordan@freygeospatial.com>
2022-08-08 11:51:07 -0400

    added some extra functionality and complexity. completed pg 13

--- Chapter1_DomainModeling/model.py
+++ Chapter1_DomainModeling/model.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
-from typing import Optional
 from datetime import date
+from typing import Optional, Set
 
+# to note:
+# a set is an unordered, unchangeable, unindexed collection. No duplicates are accepted
 
-@dataclass(frozen=True)
+@dataclass(frozen=True) # frozen dataclasses mean that an object cannot be modified!
 class OrderLine:
     orderid: str
     sku: str
@@ -17,22 +19,41 @@ class Batch:
         self.reference is a Batch ID.
         self.sku is the SKU number for the product
         self.eta is the estimated time of delivery
-        self.available_quantity is how much inventory of product is available
+        self._purchased_quantity is how much inventory of product is available
+        self._allocations is a set of OrderLines for the batch
         """
 
-        self.reference = ref
-        self.sku = sku
-        self.eta = eta
-        self.available_quantity = qty
-
+        self.reference: str = ref
+        self.sku: str = sku
+        self.eta: Optional[date] = eta
+        self._purchased_quantity: int = qty # the amount of inventory available
+        self._allocations: Set[OrderLine] = set() # of type set, which can contain OderLines
 
     # If a string is the first thing presented in a function, by
     # default it becomes the docstring. To access, use `help(Batch.allocate)`
-    def allocate(self, line: OrderLine):
+    def allocate(self, line: OrderLine) -> None:
         """
-        remove inventory after order is placed
+        add the orderline to the set
         """
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line) # .add() is a method available to sets
+    
+    def deallocate(self, line: OrderLine) -> None:
+        """
+        remove the allocation / orderline from the set (maybe order was cancelled?)
+        """
+        if line in self._allocations:
+            self._allocations._allocations.remove(line) # .remove() is a method available to sets
+
+    @property
+    def allocated_quantity(self) -> int:
+        """sums the quantity of allocations"""
+        return sum(line.qty for line in self._allocations)
+    
+    @property
+    def available_quantity(self) -> int:
+        """finds the remaining batch inventory after order lines have been placed"""
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         """
