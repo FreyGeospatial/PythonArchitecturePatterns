5c31ae06af73b008935e2edfe4a67d6255fc1418
FreyGeospatial <jordan@freygeospatial.com>
2022-08-08 14:58:04 -0400

    more examples of value obj and entities

--- Chapter1_DomainModeling/test_value_object.py
+++ Chapter1_DomainModeling/test_value_object.py
@@ -1,5 +1,5 @@
 import pytest
-from value_object import Money
+from value_object import Money, Name, Person
 
 fiver = Money('usd', 5)
 tenner = Money('usd', 10)
@@ -13,3 +13,17 @@ def can_add_money_values_for_the_same_currency():
 def adding_different_currencies_fails():
     with pytest.raises(ValueError):
         assert Money('gbp', 10) + Money('usd', 10)
+
+
+# This is a value object, 
+def test_name_equality():
+    assert Name("Harry", "Percival") != Name("Barry", "Percival")
+
+
+def test_barry_is_harry():
+    harry = Person(Name("Harry", "Percival"))
+    barry = harry
+
+    barry.name = Name("Barry", "Percival")
+
+    assert harry is barry and barry is harry
